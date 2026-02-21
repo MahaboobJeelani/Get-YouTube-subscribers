0b2b144c9267a184c132559fede663f1cb4c266b
mahaboob jeelani basha <mahaboob@murphi.ai>
2026-02-21 15:46:17 +0530

    added dynamic URI

--- src/swagger.js
+++ src/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:3000`,     
+        url: `${process.env.URI}`,     
         description: 'Development server',
       },
     ],
